8892bbba8e06d784c2d7e3476ae283f104bed3e4
Passo1114 <renka09banira07@gmail.com>
2024-09-12 19:18:45 +0900

    ファイルをアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+
+const btn = document.getElementById('btn');
+
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理
+// 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました';
+}, 2000);
+});
+
